08fcb7d7b998a29530245c65f9f0fe7514a2e4e3
Ryan Schuda <42558787+RyanSchuda@users.noreply.github.com>
2019-01-24 13:21:55 -0600

    Add files via upload

--- SitePrototype_Ryan/js/main.js
+++ SitePrototype_Ryan/js/main.js
@@ -14,11 +14,26 @@ function runSearch(){
 
 var search = getParameterByName('search');
 if (search){
+	document.getElementById('search').value=search
 	
+	document.getElementById("responses").innerHTML=""
 	if (search.match(/hello/gi)){
-		
+		document.getElementById("responses").innerHTML=document.getElementById("responses").innerHTML+`
+		<div class="response">
+		<h3>Hello World is world's most popular statement</h3>
+		<p>Hello World has spread across the world as the most popular statement, especially in coding experiences</p>
+		</div>
+		`
 	}
-	
+	if (search.match(/weather/gi)){
+		document.getElementById("responses").innerHTML=document.getElementById("responses").innerHTML+`
+		<div class="response">
+		<h3>University of St. Francis Weather Policy</h3>
+		<p>Sometimes falling water may impact university activities and possibly effect open hours.</p>
+		</div>
+		`
+	}
+	/*
 	document.getElementById("responses").innerHTML=`
 	<div class="response">
 	<h3>`+search+`</h3>
@@ -40,7 +55,7 @@ if (search){
 	<h3>Response Title</h3>
 	<p>Response Text</p>
 	</div>
-	`
+	`*/
 	
 }	
 	
